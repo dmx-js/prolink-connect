7c8248033a72ad404a7705967802377e262760e2
Alistair Smith <hi@alistair.sh>
2024-05-12 22:50:54 +0100

    remove all tracing

--- src/localdb/utils.ts
+++ src/localdb/utils.ts
@@ -4,18 +4,18 @@ import {CueAndLoop, HotcueButton} from 'src/types';
  * Create a CueAndLoop entry given common parameters
  */
 export const makeCueLoopEntry = (
-  isCue: boolean,
-  isLoop: boolean,
-  offset: number,
-  length: number,
-  button: false | HotcueButton
+	isCue: boolean,
+	isLoop: boolean,
+	offset: number,
+	length: number,
+	button: false | HotcueButton,
 ): null | CueAndLoop =>
-  button !== false
-    ? isLoop
-      ? {type: 'hot_loop', offset, length, button}
-      : {type: 'hot_cue', offset, button}
-    : isLoop
-    ? {type: 'loop', offset, length}
-    : isCue
-    ? {type: 'cue_point', offset}
-    : null;
+	button !== false
+		? isLoop
+			? {type: 'hot_loop', offset, length, button}
+			: {type: 'hot_cue', offset, button}
+		: isLoop
+			? {type: 'loop', offset, length}
+			: isCue
+				? {type: 'cue_point', offset}
+				: null;
